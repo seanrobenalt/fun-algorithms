6ef2d88ee6a89bcb37dc17b9fd61642ec71eb206
Sean Robenalt <seanaltonrobenalt@gmail.com>
2017-12-01 20:34:54 -0500

    more solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,53 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// RECURSIVE SOLUTION
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
+
+/*
+
+ONE OPTION - ITERATIVE
+
+function fib(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    const a = result[i - 1];
+    const b = result[i - 2];
+
+    result.push(a + b);
+  }
+
+  return result[n];
+}
+
+RUNTIME
+for every increase in n, we do one more calculation
+so it's linear run time
+
+*/
 
 module.exports = fib;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+/*
+
+ONE OPTION
+
+function vowels(str) {
+  let count = 0;
+  let targetChar = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()) {
+    if (targetChar.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+*/
 
 module.exports = vowels;
