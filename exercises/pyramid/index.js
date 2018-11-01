682661453ddf45066f7740b3cd138717e6886c1c
Sean Robenalt <srob0722@gmail.com>
2018-10-31 23:44:22 -0400

    pyramid recursive refactor

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,7 @@
 //       ' ### '
 //       '#####'
 
+/* Iterative solution
 function pyramid(n) {
   const midpoint = Math.floor((2 * n - 1) / 2);
 
@@ -29,6 +30,28 @@ function pyramid(n) {
     }
     console.log(level);
   }
+}
+*/
+
+// Recursive solution
+function pyramid(n, row = 0, level = '') {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
